70e09dd69568d6ef5a7b23361285f07e0897b34d
Shah Sanzida Masiat <101643078+masiat11@users.noreply.github.com>
2022-07-18 14:50:21 +0600

    done

--- FinalLabTask1/task.php
+++ FinalLabTask1/task.php
@@ -1,1 +1,39 @@
 
+<?php 
+
+    $conn = mysqli_connect('localhost', 'root', '', 'web');
+
+    // if($conn){
+    //     echo "Success";
+    // }else{
+    //     echo "DB Error...";
+    // }
+
+    $sql = "select * from admin";
+    $result = mysqli_query($conn, $sql);
+    
+  echo  "<table border ='2'>";
+  echo   "<tr>";
+  echo   "<th>Id</th>";
+  echo   "<th>Name</th>";
+  echo "<th>Password</th>";
+  echo  "</tr>";
+ 
+
+
+
+while($row= mysqli_fetch_assoc($result)){
+      echo "<tr><td>";
+      echo $row['Id']."</td>";
+      echo "<td>".$row['Name']."</td>";
+      echo "<td>". $row['Password'] ."</td>";
+
+
+    
+
+     
+       
+    }
+
+     echo "</table>";
+?>
